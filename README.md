d4039037597ca5e1060fa33c9d27b62d566c410d
VMware Developer Center <noreply@vmware.com>
2016-10-13 00:11:05 +0000

    Initial commit by sampleminion

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# deleteme-duplicate-sample
+This is a demo with a single java file in it
---
+++ SomeFile.java
@@ -0,0 +1,7 @@
+// nothing to see here, this is just test code
+package com.vmware.dc.samples.test
+
+class SomeFile {
+	public SomeFile() {
+}
+}
